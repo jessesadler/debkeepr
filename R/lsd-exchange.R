c2c267edab274ba2da7f15e62d5433072c95d048
Jesse Sadler <jsadler@ucla.edu>
2018-06-10 17:17:28 -0700

    Create deb_lsd_exchange_mutate

--- R/lsd-exchange.R
+++ R/lsd-exchange.R
@@ -3,28 +3,38 @@
 # Exchange rate by shillings
 # This is set up to go from sterling to vlaams
 deb_lsd_exchange <- function(l, s, d,
-                             rate = 31,
+                             rate_per_solidi,
                              round = 3,
                              vector = FALSE) {
-  denarii <- deb_lsd_d(l, s, d) * rate/20
-  deb_d_lsd(denarii, round, vector)
+  deb_multiply(x = rate_per_solidi/20,
+               l, s, d,
+               round = round,
+               vector = vector)
 }
 
-# Exchange rate of ducats
-# From ducats to lsd with rate as denarii per ducat
-deb_ducats_lsd <- function(ducats,
-                           deniers = 0,
-                           rate = 96,
-                           round = 3,
-                           vector = FALSE) {
-  deniers <- deniers/24
-  deb_d_lsd((ducats + deniers) * rate, round, vector)
-}
 
-# From lsd to ducats with rate as denarii per ducat
-deb_lsd_ducats <- function(l, s, d, rate = 96) {
-  denari <- deb_lsd_d(l, s, d) * 24/rate
+deb_lsd_exchange_mutate <- function(df,
+                                    l = l,
+                                    s = s,
+                                    d = d,
+                                    rate_per_solidi,
+                                    suffix = ".1",
+                                    round = 3) {
+  l <- rlang::enquo(l)
+  s <- rlang::enquo(s)
+  d <- rlang::enquo(d)
 
-  tibble(ducats = denari %/% 24,
-         denari = denari %% 24)
+  # Checks
+  lsd_column_check(df, l, s, d)
+  # Column names: avoid overwriting l, s, and d columns
+  lsd_names <- lsd_column_names(df, l, s, d, suffix)
+
+  x <- rate_per_solidi/20
+
+  lsd_mutate_columns(df,
+                     !! l * x,
+                     !! s * x,
+                     !! d * x,
+                     lsd_names,
+                     round = round)
 }
