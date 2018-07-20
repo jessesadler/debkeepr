63d9ffb1957aa8718167c053339cea6d8ea655ab
Jesse Sadler <jsadler@ucla.edu>
2018-07-19 21:29:49 -0700

    Rewrite lsd-mutate functions
    * Change data frame decimalization to lsd functions to use lsd_mutate_columns and no longer use its own separate normalization functions.
    * Move the functions from lsd-mutate.R to lsd-decimalization-df.R. This makes lsd-decimalization-df mimic the functions in lsd-decimalization.
    * This change makes it possible to mutate columns that have NA values and prepares the mutate functions for further changes.

--- R/lsd-decimalization-df.R
+++ R/lsd-decimalization-df.R
@@ -55,6 +55,83 @@ deb_lsd_l_mutate <- function(df,
   dplyr::mutate(df, !! column_name := decimalize_l(!! l, !! s, !! d, lsd_bases))
 }
 
+#' Mutate decimal pounds into pounds, shillings, and pence variables
+#'
+#' Uses [dplyr::mutate()] to add equivalent pounds, shillings, and pence
+#' variables to a data frame that contains a decimalized pounds variable.
+#' Thus, the function converts decimalized pounds into the lsd system of
+#' pounds, shillings, and pence.
+#'
+#' @param df A data frame that contains a column of decimalized pounds.
+#' @param librae Decimalized pounds column: Unquoted name of a numeric
+#'   variable corresponding to pounds. This is the variable that will be
+#'   mutated into pounds, shillings, and pence variables.
+#' @param l_column An unquoted name for the pounds column created by the
+#'   function. Default is l.
+#' @param s_column An unquoted name for the shillings column created by
+#'   the function. Default is s.
+#' @param d_column An unquoted name for the pence column created by the
+#'   function. Default is d.
+#' @inheritParams deb_normalize_df
+#' @param suffix If the data frame already contains variables with the same
+#'   names as `l_column`, `s_column`, or `d_column`, this suffix will be
+#'   added to the new variables to distinguish them. Default is ".1".
+#'
+#' @return Returns a data frame with three new variables of pounds,
+#'   shillings, and pence.
+#'
+#' @examples
+#' # Create equivalent pounds, shillings, and pence
+#' # variables from a decimalized pounds variable
+#' example <- data.frame(pounds = c(8, 8.325, -8.325, 5.425, 4.5678))
+#'
+#' deb_l_lsd_mutate(df = example, librae = pounds)
+#'
+#' # You can choose the names for the created columns
+#' example %>%
+#'   deb_l_lsd_mutate(librae = pounds,
+#'                    l_column = librae,
+#'                    s_column = solidi,
+#'                    d_column = denarii)
+#'
+#' @export
+
+deb_l_lsd_mutate <- function(df, librae,
+                             l_column = l,
+                             s_column = s,
+                             d_column = d,
+                             lsd_bases = c(20, 12),
+                             suffix = ".1") {
+
+  librae <- rlang::enquo(librae)
+
+  # Check that librae exists in df
+  if (rlang::quo_name(librae) %in% names(df) == FALSE) {
+    stop(call. = FALSE, "librae column must exist in df")
+  }
+
+  # Check that librae is numeric
+  if (!is.numeric(rlang::eval_tidy(librae, df))) {
+    stop(call. = FALSE, "librae must be numeric")
+  }
+
+  # Column names: avoid overwriting l, s, and d columns
+  suffix <- suffix_check(suffix)
+  lsd_names <- lsd_column_names(df,
+                                rlang::enquo(l_column),
+                                rlang::enquo(s_column),
+                                rlang::enquo(d_column),
+                                suffix)
+
+  lsd_mutate_columns(df,
+                     l = !! librae,
+                     s = rep(0, length(!! librae)),
+                     d = rep(0, length(!! librae)),
+                     lsd_names = lsd_names,
+                     replace = FALSE,
+                     lsd_bases = lsd_bases)
+}
+
 #' Convert from pounds, shillings and pence to shillings
 #'
 #' Convert pounds, shillings, and pence variables in a data frame to a
@@ -93,6 +170,73 @@ deb_lsd_s_mutate <- function(df,
   column_name <- rlang::quo_name(column_name)
 
   dplyr::mutate(df, !! column_name := decimalize_s(!! l, !! s, !! d, lsd_bases))
+}
+
+#' Mutate decimal shillings into pounds, shillings, and pence variables
+#'
+#' Uses [dplyr::mutate()] to add equivalent pounds, shillings, and pence
+#' variables to a data frame that contains a decimalized shillings variable.
+#' Thus, the function converts decimalized shillings into the lsd system of
+#' pounds, shillings, and pence.
+#'
+#' @param df A data frame that contains a column of decimalized shillings.
+#' @param solidi Decimalized shillings column: Unquoted name of a numeric
+#'   variable corresponding to shillings. This is the variable that will be
+#'   mutated into pounds, shillings, and pence variables.
+#' @inheritParams deb_l_lsd_mutate
+#'
+#' @return Returns a data frame with three new variables of pounds, shillings,
+#'   and pence.
+#'
+#' @examples
+#' # Create equivalent pounds, shillings, and pence
+#' # variables from a decimalized shillings variable
+#' example <- data.frame(shillings = c(166, -166, 166.5, 236.35, -354.845))
+#'
+#' deb_s_lsd_mutate(df = example, solidi = shillings)
+#'
+#' # You can choose the names for the created columns
+#' example %>%
+#'   deb_s_lsd_mutate(solidi = shillings,
+#'                    l_column = librae,
+#'                    s_column = solidi,
+#'                    d_column = denarii)
+#'
+#' @export
+
+deb_s_lsd_mutate <- function(df, solidi,
+                             l_column = l,
+                             s_column = s,
+                             d_column = d,
+                             lsd_bases = c(20, 12),
+                             suffix = ".1") {
+  solidi <- rlang::enquo(solidi)
+
+  # Check that solidi exists in df
+  if (rlang::quo_name(solidi) %in% names(df) == FALSE) {
+    stop(call. = FALSE, "solidi column must exist in df")
+  }
+
+  # Check that solidi is numeric
+  if (!is.numeric(rlang::eval_tidy(solidi, df))) {
+    stop(call. = FALSE, "solidi must be numeric")
+  }
+
+  # Column names: avoid overwriting l, s, and d columns
+  suffix <- suffix_check(suffix)
+  lsd_names <- lsd_column_names(df,
+                                rlang::enquo(l_column),
+                                rlang::enquo(s_column),
+                                rlang::enquo(d_column),
+                                suffix)
+
+  lsd_mutate_columns(df,
+                     l = rep(0, length(!! solidi)),
+                     s = !! solidi,
+                     d = rep(0, length(!! solidi)),
+                     lsd_names = lsd_names,
+                     replace = FALSE,
+                     lsd_bases = lsd_bases)
 }
 
 #' Convert from pounds, shillings and pence to pence
@@ -135,4 +279,71 @@ deb_lsd_d_mutate <- function(df,
   column_name <- rlang::quo_name(column_name)
 
   dplyr::mutate(df, !! column_name := decimalize_d(!! l, !! s, !! d, lsd_bases))
+}
+
+#' Mutate decimal pence into pounds, shillings, and pence variables
+#'
+#' Uses [dplyr::mutate()] to add equivalent pounds, shillings, and pence
+#' variables to a data frame that contains a pence variable. Thus, the
+#' function converts pence into the lsd system of pounds, shillings,
+#' and pence.
+#'
+#' @param df A data frame that contains a column of pence.
+#' @param denarii Pence column: Unquoted name of a numeric variable
+#'   corresponding to pence. This is the variable that will be
+#'   mutated into pounds, shillings, and pence variables.
+#' @inheritParams deb_l_lsd_mutate
+#'
+#' @return Returns a data frame with three new variables of pounds, shillings,
+#'   and pence.
+#'
+#' @examples
+#' # Create equivalent pounds, shillings, and pence
+#' # variables from a pence variable
+#' example <- data.frame(pence = c(1998, -1998, 387, -5378))
+#'
+#' deb_d_lsd_mutate(df = example, denarii = pence)
+#'
+#' # You can choose the names for the created columns
+#' example %>%
+#'   deb_d_lsd_mutate(denarii = pence,
+#'                    l_column = librae,
+#'                    s_column = solidi,
+#'                    d_column = denarii)
+#'
+#' @export
+
+deb_d_lsd_mutate <- function(df, denarii,
+                             l_column = l,
+                             s_column = s,
+                             d_column = d,
+                             lsd_bases = c(20, 12),
+                             suffix = ".1") {
+  denarii <- rlang::enquo(denarii)
+
+  # Check that denarii exists in df
+  if (rlang::quo_name(denarii) %in% names(df) == FALSE) {
+    stop(call. = FALSE, "denarii column must exist in df")
+  }
+
+  # Check that denarii is numeric
+  if (!is.numeric(rlang::eval_tidy(denarii, df))) {
+    stop(call. = FALSE, "denarii must be numeric")
+  }
+
+  # Column names: avoid overwriting l, s, and d columns
+  suffix <- suffix_check(suffix)
+  lsd_names <- lsd_column_names(df,
+                                rlang::enquo(l_column),
+                                rlang::enquo(s_column),
+                                rlang::enquo(d_column),
+                                suffix)
+
+  lsd_mutate_columns(df,
+                     l = rep(0, length(!! denarii)),
+                     s = rep(0, length(!! denarii)),
+                     d = !! denarii,
+                     lsd_names = lsd_names,
+                     replace = FALSE,
+                     lsd_bases = lsd_bases)
 }
--- tests/testthat/test-lsd-decimalization-df.R
+++ tests/testthat/test-lsd-decimalization-df.R
@@ -14,6 +14,8 @@ l_df <- data.frame(librae = deb_lsd_l(ex_list))
 s_df <- data.frame(solidi = deb_lsd_s(ex_list))
 d_df <- data.frame(denarii = deb_lsd_d(ex_list))
 
+## lsd to decimalized values ##
+
 test_that("individual l, s, and d helper functions work", {
   expect_equal(decimalize_l(8, 10, 6), 8.525)
   expect_equal(decimalize_s(8, 10, 6), 170.5)
@@ -29,7 +31,7 @@ test_that("NA works", {
                data.frame(l = 30, s = 10, d = as.numeric(NA), librae = as.numeric(NA)))
 })
 
-test_that("df pounds decimalization", {
+test_that("lsd df to decimalized pounds", {
   expect_equal(names(deb_lsd_l_mutate(ex_df)),
                c("l", "s", "d", "librae"))
   expect_equal(names(deb_lsd_l_mutate(ex_df_names, l = pounds, s = shillings, d = pence)),
@@ -39,7 +41,7 @@ test_that("df pounds decimalization", {
   expect_equal(deb_lsd_l_mutate(ex_df), cbind(ex_df, l_df))
 })
 
-test_that("df shillings decimalization", {
+test_that("lsd df to decimalized shillings", {
   expect_equal(names(deb_lsd_s_mutate(ex_df)),
                c("l", "s", "d", "solidi"))
   expect_equal(names(deb_lsd_s_mutate(ex_df_names, l = pounds, s = shillings, d = pence)),
@@ -49,7 +51,7 @@ test_that("df shillings decimalization", {
   expect_equal(deb_lsd_s_mutate(ex_df), cbind(ex_df, s_df))
 })
 
-test_that("df pence decimalization", {
+test_that("lsd df to decimalized pence", {
   expect_equal(names(deb_lsd_d_mutate(ex_df)),
                c("l", "s", "d", "denarii"))
   expect_equal(names(deb_lsd_d_mutate(ex_df_names, l = pounds, s = shillings, d = pence)),
@@ -57,4 +59,70 @@ test_that("df pence decimalization", {
   expect_equal(names(deb_lsd_d_mutate(ex_df, column_name = pence)),
                c("l", "s", "d", "pence"))
   expect_equal(deb_lsd_d_mutate(ex_df), cbind(ex_df, d_df))
+})
+
+## Decimalized values to lsd ##
+v_decimal <- c(327.873, 683.33333, 683.33333333, -645.865, 11.999999, NA)
+
+df_decimal <- data.frame(decimal = v_decimal)
+l_decimal <- data.frame(decimal = v_decimal,
+                        l = c(327, 683, 683, -645, 11, as.numeric(NA)),
+                        s = c(17, 6, 6, -17, 19, as.numeric(NA)),
+                        d = c(5.52, 7.9992, 8, -3.6, 11.99976, as.numeric(NA)))
+s_decimal <- data.frame(decimal = v_decimal,
+                        l = c(16, 34, 34, -32, 0, as.numeric(NA)),
+                        s = c(7, 3, 3, -5, 11, as.numeric(NA)),
+                        d = c(10.476, 3.99996, 4, -10.38, 11.99999, as.numeric(NA)))
+d_decimal <- data.frame(decimal = v_decimal,
+                        l = c(1, 2, 2, -2, 0, as.numeric(NA)),
+                        s = c(7, 16, 16, -13, 1, as.numeric(NA)),
+                        d = c(3.873, 11.33333, 11.33333, -9.865, 0, as.numeric(NA)))
+
+test_that("decimal column exists", {
+  expect_error(deb_l_lsd_mutate(df_decimal, librae = hello),
+               "librae column must exist in df")
+  expect_error(deb_s_lsd_mutate(df_decimal, solidi = hello),
+               "solidi column must exist in df")
+  expect_error(deb_d_lsd_mutate(df_decimal, denarii = hello),
+               "denarii column must exist in df")
+})
+
+id_df <- data.frame(id = letters[1:3])
+
+test_that("decimal column is numeric", {
+  expect_error(deb_l_lsd_mutate(id_df, librae = id),
+               "librae must be numeric")
+  expect_error(deb_s_lsd_mutate(id_df, solidi = id),
+               "solidi must be numeric")
+  expect_error(deb_d_lsd_mutate(id_df, denarii = id),
+               "denarii must be numeric")
+})
+
+test_that("Decimalized values df to lsd", {
+  expect_equal(deb_l_lsd_mutate(df_decimal, decimal), l_decimal)
+  expect_false(identical(deb_l_lsd_mutate(df_decimal, decimal),
+                         deb_l_lsd_mutate(df_decimal, decimal, lsd_bases = c(8, 16))))
+
+  expect_equal(deb_s_lsd_mutate(df_decimal, decimal), s_decimal)
+  expect_false(identical(deb_s_lsd_mutate(df_decimal, decimal),
+                         deb_s_lsd_mutate(df_decimal, decimal, lsd_bases = c(8, 16))))
+
+  expect_equal(deb_d_lsd_mutate(df_decimal, decimal), d_decimal)
+  expect_false(identical(deb_d_lsd_mutate(df_decimal, decimal),
+                         deb_d_lsd_mutate(df_decimal, decimal, lsd_bases = c(8, 16))))
+})
+
+test_that("column names change with lsd_column_names function", {
+  # Can change names
+  expect_equal(names(deb_l_lsd_mutate(df_decimal, decimal,
+                                      l_column = "librae",
+                                      s_column = "solidi",
+                                      d_column = "denarii")),
+               c("decimal", "librae", "solidi", "denarii"))
+  # Suffix added if names already present
+  expect_equal(names(deb_s_lsd_mutate(s_decimal, decimal)),
+               c("decimal", "l", "s", "d", "l.1", "s.1", "d.1"))
+  # Can change suffix
+  expect_equal(names(deb_d_lsd_mutate(s_decimal, decimal, suffix = ".x")),
+               c("decimal", "l", "s", "d", "l.x", "s.x", "d.x"))
 })
