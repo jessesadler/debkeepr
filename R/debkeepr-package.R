faa0b80a964af2f8417a92e3047b017afb508283
Jesse Sadler <jsadler@ucla.edu>
2019-05-21 11:40:05 -0700

    Change to use of debkeepr-package.R
    * Move utils to debkeepr-package.R
    * Take out tidyeval and pipe utils. These will likely come back in the package, but they are not needed now.

--- R/debkeepr-package.R
+++ R/debkeepr-package.R
@@ -1,4 +1,11 @@
-## Utils ##
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
 
 #' Unpacking assignment
 #'
